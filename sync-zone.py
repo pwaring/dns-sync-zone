2c1ae17cc050b4687d683d1a432cf537254141c2
Paul Waring <paul@xk7.net>
2017-10-26 12:29:39 +0100

    Validate zone records and skip where necessary
    Comments are now supported, if a line begins with '#' it will be
    skipped.
    
    Empty lines are skipped.
    
    Basic validation is attempted on all non-skipped zone records.
    
    Closes #1

--- sync-zone.py
+++ sync-zone.py
@@ -1,9 +1,73 @@
 # Standard library modules
 import argparse
 import json
+import sys
 
 # External modules
 import requests
+
+"""
+Whether a zone record should be skipped or not.
+Records are skipped if they are empty or start with a # (indicating a comment).
+
+:param zone_record: Full zone record, including the command (e.g. ADD).
+:returns: True if the record should be skipped, false otherwise.
+"""
+def skip_zone_record(zone_record):
+    zone_record = zone_record.strip()
+
+    return (not zone_record or zone_record[:1] == '#')
+
+"""
+Validate a zone record.
+This validation is not exhaustive and may allow some invalid records through
+(false positive) or reject valid records (false negative). It is mainly
+intended to catch common errors, although in time it will be expanded.
+
+:param zone_record: Full zone record, including the command (e.g. ADD).
+:returns: True if the record appears valid, false otherwise.
+"""
+def validate_zone_record(zone_record):
+    valid_commands = ['ADD', 'DELETE', 'REPLACE']
+
+    # Only a subset of all valid types are supported
+    valid_types_basic = ['A', 'CNAME', 'AAAA']
+    valid_types_extra = ['MX', 'TXT']
+
+    if skip_zone_record(zone_record):
+        return True
+    else:
+        # Looks like an actual record
+        zone_record = zone_record.strip()
+        zone_record_parts = zone_record.split()
+
+        if len(zone_record_parts) >= 5:
+            # First four fields are the same for all records
+            record_command = zone_record_parts[0]
+            record_hostname = zone_record_parts[1]
+            record_ttl = zone_record_parts[2]
+            record_type = zone_record_parts[3]
+
+            # Command, TTL and type are required for all records
+            if record_command in valid_commands and record_ttl.isdigit() and (record_type in valid_types_basic or record_type in valid_types_extra):
+                if len(zone_record_parts) == 5:
+                    # Looks like a standard record type
+                    record_data = zone_record_parts[4]
+
+                    if record_type in valid_types_basic or record_type == 'TXT':
+                        return True
+                elif len(zone_record_parts) == 6 and record_type == 'MX':
+                    record_priority = zone_record_parts[4]
+                    record_data = zone_record_parts[5]
+
+                    if record_priority.isdigit():
+                        return True
+                elif len(zone_record_parts) >= 5 and record_type == 'TXT':
+                    record_data = zone_record_parts[4:]
+                    return True
+
+    return False
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--credentials-file', help='path to credentials file', required=True)
@@ -11,11 +75,18 @@ parser.add_argument('--zone', help='name of zone (e.g. example.org)', required=T
 parser.add_argument('--zone-file', help='path to zone file', required=True)
 args = parser.parse_args()
 
-with open(args.credentials_file) as f:
-    credentials = json.load(f)
-
 with open(args.zone_file) as f:
     zone_records = f.read()
+
+# Validate all new zone records
+for zone_record in zone_records.splitlines():
+    if not validate_zone_record(zone_record):
+        print("The following record failed validation:")
+        print(zone_record)
+        sys.exit(1)
+
+with open(args.credentials_file) as f:
+    credentials = json.load(f)
 
 base_payload = {
   'domain': args.zone,
@@ -38,8 +109,10 @@ delete_records = []
 
 for list_record in list_records:
     record_parts = list_record.split()
+    record_type = record_parts[2]
 
-    if record_parts[2] != 'NS':
+    # Do not delete NS or SOA records as this may break the zone
+    if record_type != 'NS' and record_type != 'SOA':
         delete_records.append(list_record)
 
 delete_commands = []
@@ -50,7 +123,8 @@ for delete_record in delete_records:
 # Send all the DELETE and new zone entries in one transaction
 sync_commands = delete_commands
 for zone_record in zone_records.splitlines():
-    sync_commands.append(zone_record)
+    if not skip_zone_record(zone_record):
+        sync_commands.append(zone_record)
 
 print(sync_commands)
 
